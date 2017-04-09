2dd67a7ebb5e43a3e93a2846b49082cf751b3de2
wangchao <wang_chao@corp.netease.com>
2017-04-09 13:52:54 +1200

    Initial Commit

---
+++ HLSPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLSPlayer
+//
+//  Created by Charles on 9/04/17.
+//  Copyright © 2017 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLSPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLSPlayer
+//
+//  Created by Charles on 9/04/17.
+//  Copyright © 2017 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
