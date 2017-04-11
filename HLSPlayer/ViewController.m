e00abcf081e953e7b00d5dbffd3c233555428fa0
wangchao <wang_chao@corp.netease.com>
2017-04-11 14:17:58 +1200

    first commit

--- HLSPlayer/ViewController.m
+++ HLSPlayer/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate>
 
 @end
 
@@ -17,6 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
@@ -25,5 +28,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - TableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+
+    AVPlayerViewController *avVC = [[AVPlayerViewController alloc]init];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    __block NSString *urlString = @"";
+    [cell.contentView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj isKindOfClass:[UILabel class]]) {
+            urlString = ((UILabel *)obj).text;
+            *stop = YES;
+        }
+    }];
+    
+    avVC.player = [[AVPlayer alloc]initWithURL:[NSURL URLWithString:urlString]];
+    [self presentViewController:avVC animated:YES completion:^{
+        [avVC.player play];
+    }];
+}
 
 @end
